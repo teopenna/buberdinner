341daf43b0b2e7a54bf23e24a303dd7a11780da0
Matteo Penna <penna.matteo@gmail.com>
2023-09-02 21:19:42 +0200

    Added problem details factory

--- src/BuberDinner.Api/Program.cs
+++ src/BuberDinner.Api/Program.cs
@@ -1,11 +1,16 @@
+using BuberDinner.Api.Errors;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddApplication().AddInfrastructure(builder.Configuration);
+
 builder.Services.AddControllers();
+
+builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 
 var app = builder.Build();
 
